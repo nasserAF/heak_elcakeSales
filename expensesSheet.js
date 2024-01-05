ab8425b476b849e4161d0cd6f6a69723ed121392
nasserAF <nasser_af73@hotmail.com>
2024-01-05 09:51:45 +0300

    Add files via upload

---
+++ expensesSheet.js
@@ -0,0 +1,11 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbzGk3A6TO790DZc0cJUIvgJo5PHpxsO1kyIQt_4471bkZQwkkonzkuBCbeXdxP-GCxu/exec'
+
+const form = document.forms['expensesForm']
+
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+  .then(response => alert("شكرا لك ،،، تمت العملية بنجاح" ))
+  .then(() => { window.location.reload(); })
+  .catch(error => console.error('Error!', error.message))
+})
