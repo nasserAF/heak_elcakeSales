3a81f50d474b3266c7eda4be938f782f25edffc7
nasserAF <nasser_af73@hotmail.com>
2024-04-17 18:27:57 +0300

    Update viewRecruitments.js

--- shopOnly/viewRecruitments.js
+++ shopOnly/viewRecruitments.js
@@ -1,4 +1,4 @@
-const webAppUrl = 'https://script.google.com/macros/s/AKfycbxux3Ak3wpzh_U5E5ss4w7z7rmDeLsCTBcbPKR84XqnltUvNvGdM42AGtZDNs3JU18mqw/exec'; // Replace with your Apps Script URL
+const webAppUrl = 'https://script.google.com/macros/s/AKfycbw7V6nwsyTVgZnQWkTupmz6963vHumS6s7bpn2Xd9QXsXS7KC8aS6U7gZDq_Cp6AgE1bw/exec'; // Replace with your Apps Script URL
 
 const translations = {
     fullName: 'الاسم الرباعي',
